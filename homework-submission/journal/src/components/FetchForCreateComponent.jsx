4043e6b8bad6b45dc086e311fa107804e902c13c
hakanErgin <hakan.ergin@gmail.com>
2020-01-03 17:18:48 +0100

    ready for netlify deploy

--- homework-submission/journal/src/components/FetchForCreateComponent.jsx
+++ homework-submission/journal/src/components/FetchForCreateComponent.jsx
@@ -1,4 +1,8 @@
+import DetailComponent from './DetailComponent'
+import React, { useState } from 'react'
+
 const FetchForListComponent = ({ title, body }) => {
+  const [apiResults, setApiResults] = useState(null)
 
   let url = 'https://jsonplaceholder.typicode.com/posts'
 
@@ -11,11 +15,15 @@ const FetchForListComponent = ({ title, body }) => {
     body: JSON.stringify({ title, body })
   })
     .then(res => res.json())
-    .then(response => {
-      console.log(response[0].body, response[0].title, response[0].id)
-    })
+    .then(res => setApiResults(res))
 
-  return (null)
+  if (!apiResults) return null
+
+  return (
+    <div>
+      <br />
+      <DetailComponent data={apiResults} /></div>
+  )
 }
 
 export default FetchForListComponent
